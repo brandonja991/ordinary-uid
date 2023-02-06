1f056bff2ed644c36e9f641678d2ea3091437eda
Brandon Jackson <brandonja991@gmail.com>
2023-02-06 11:39:23 -0600

    Add datetime to uid interface

--- src/UIDInterface.php
+++ src/UIDInterface.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Ordinary\UID;
 
+use DateTimeImmutable;
+
 interface UIDInterface
 {
+    public function dateTime(): DateTimeImmutable;
+
     public function value(): string;
 
     public function timeSeconds(): int;
